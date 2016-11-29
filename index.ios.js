47ee2d68994c694ab860c56e06d189fd4cbb08ec
Golden Wish LLC <cpenny@goldenwishllc.com>
2016-11-29 13:46:39 -0500

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,51 @@
+/**
+ * The examples provided by Facebook are for non-commercial testing and
+ * evaluation purposes only.
+ *
+ * Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @providesModule UIExplorerApp
+ * @flow
+ */
+'use strict';
+
+var React = require('react-native');
+var UIExplorerList = require('./UIExplorerList');
+var {
+  AppRegistry,
+  NavigatorIOS,
+  StyleSheet,
+} = React;
+
+var UIExplorerApp = React.createClass({
+
+  getInitialState: function() {
+    return {
+      openExternalExample: (null: ?React.Component),
+    };
+  },
+
+  render: function() {
+     return <UIExplorerList/>;
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  itemWrapper: {
+    backgroundColor: '#eaeaea',
+  },
+});
+
+AppRegistry.registerComponent('UIExplorerApp', () => UIExplorerApp);
+
+module.exports = UIExplorerApp;
